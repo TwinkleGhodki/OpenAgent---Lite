8e81d42c863a34e720d6214b52a9a2f875b56eb1
TwinkleGhodki <160323542+TwinkleGhodki@users.noreply.github.com>
2025-06-29 23:15:40 +0530

    logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,12 @@
 import os
 import datetime
 
-# Get absolute path to logs folder
+# Getting absolute path to logs folder
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG_DIR = os.path.join(BASE_DIR, 'logs')
 LOG_FILE = os.path.join(LOG_DIR, 'execution.log')
 
-# Make sure logs directory exists
+# Making sure logs directory exists
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
